9faeee4ae02a44aa32373b019cdbe90485c3ff71
pix3l <dumail521@gmail.com>
2020-04-03 16:10:05 +0200

    Update credcap.py

--- credcap.py
+++ credcap.py
@@ -12,42 +12,42 @@ def readPacket(packet_file):
     pass_re = r'(?i)(?:password|passwd|pass)[=:\s]{1,2}(\w{2,200})' #re.findall(pass_re, packet.load)[0][3]
 
     try:
-        packets = rdpcap(packet_file)
+        streams = rdpcap(packet_file).sessions()
 
     except scapy.error.Scapy_Exception:
         errorPcap()
 
     all_packets = []
     packet_data = {}
+    for stream in streams:
+        for packet in streams[stream]:
+            if 'Raw' in packet:
+                domain_re = re.findall(r'((?!:\/\/)(\w+\.)*\w+\.\w{2,11})', packet.load)
+                user = re.findall(user_re, packet.load)
+                passwd = re.findall(pass_re, packet.load)
+                if user != [] and 'username' not in packet_data:
+                    username = user[0]
+                    packet_data['username'] = username
+                    packet_data['src_ip'] = packet['IP'].src
+                    packet_data['dst_ip'] = packet['IP'].dst
+                    print('Found username: {}'.format(username))
 
-    for packet in packets:
-        if 'Raw' in packet:
-            domain_re = re.findall(r'((?!:\/\/)(\w+\.)*\w+\.\w{2,11})', packet.load)
-            user = re.findall(user_re, packet.load)
-            passwd = re.findall(pass_re, packet.load)
-            if user != [] and 'username' not in packet_data:
-                username = user[0]
-                packet_data['username'] = username
-                packet_data['src_ip'] = packet['IP'].src
-                packet_data['dst_ip'] = packet['IP'].dst
-                print('Found username: {}'.format(username))
+                if passwd != [] and 'password' not in packet_data:
+                    password = passwd[0]
+                    packet_data['password'] = password
+                    packet_data['src_ip'] = packet['IP'].src
+                    packet_data['dst_ip'] = packet['IP'].dst
+                    print('Found password: {}'.format(password))
 
-            if passwd != [] and 'password' not in packet_data:
-                password = passwd[0]
-                packet_data['password'] = password
-                packet_data['src_ip'] = packet['IP'].src
-                packet_data['dst_ip'] = packet['IP'].dst
-                print('Found password: {}'.format(password))
+                if domain_re != [] and 'domain' not in packet_data:
+                    domain = domain_re[0][0]
+                    packet_data['domain'] = domain
+                    packet_data['src_ip'] = packet['IP'].src
+                    packet_data['dst_ip'] = packet['IP'].dst
 
-            if domain_re != [] and 'domain' not in packet_data:
-                domain = domain_re[0][0]
-                packet_data['domain'] = domain
-                packet_data['src_ip'] = packet['IP'].src
-                packet_data['dst_ip'] = packet['IP'].dst
-
-        elif len(packet_data) >= 4:
-            all_packets.append(packet_data)
-            packet_data = {}
+            elif len(packet_data) >= 4:
+                all_packets.append(packet_data)
+                packet_data = {}
 
     return all_packets
 
